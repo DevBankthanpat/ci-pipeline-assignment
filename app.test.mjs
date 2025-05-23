176ba6fc3cfb30db9de2993c71b87bf6ae5dec88
thanapat munmanothum <bankthanapat@gmail.com>
2025-05-23 19:24:01 +0700

    Update app.test.mjs

--- app.test.mjs
+++ app.test.mjs
@@ -5,7 +5,7 @@ describe("GET /", () => {
   it("should return Hello TechUp!", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toHaveProperty("Hello TechUp!");
+    expect(res.body).toHaveProperty("Hello TechUp!");
   });
 });
 
