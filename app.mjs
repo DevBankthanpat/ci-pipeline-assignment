ecd0174577f0658e41c9643b8ec91b3ac0d0f53b
thanapat munmanothum <bankthanapat@gmail.com>
2025-05-23 19:29:28 +0700

    test

--- app.mjs
+++ app.mjs
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ "Hello TechUp!": "OK" });
 });
 
 app.get("/posts", (req, res) => {
